47108a4bddb9c1b30850944d6e629fec5df36345
domagoj.vukovic <off.vukovic@gmail.com>
2021-02-12 00:43:30 +0100

    refactor(Login): code cleanup and aligned content on the screen

--- src/modules/Login/Login.tsx
+++ src/modules/Login/Login.tsx
@@ -4,37 +4,50 @@ import {
 } from '@react-native-community/google-signin'
 import auth from '@react-native-firebase/auth'
 import * as React from 'react'
-import { View } from 'react-native'
+import {
+    StyleSheet,
+    Text,
+    View,
+} from 'react-native'
+
+const styles = StyleSheet.create({
+    button: {
+        height: 48,
+        width: 192,
+    },
+    root: {
+        alignItems: 'center',
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100%',
+        justifyContent: 'center',
+        padding: 20,
+    },
+    title: {
+        fontSize: 30,
+        marginBottom: 50,
+    },
+})
 
 export const Login: React.FunctionComponent = () => {
-    async function onGoogleButtonPress() {
-        // Get the users ID token
+    const handleSignIn = async () => {
         const { idToken } = await GoogleSignin.signInSilently()
 
-        // Create a Google credential with the token
         const googleCredential = auth.GoogleAuthProvider.credential(idToken)
 
-        // Sign-in the user with the credential
-        return auth().signInWithCredential(googleCredential)
-    }
-
-    const handleSignIn = async () => {
-        await onGoogleButtonPress()
-            .then(() => {
-                console.log('Signed in with Google!')
-            })
-            .catch((error) => {
-                console.log(error)
-            })
+        await auth().signInWithCredential(googleCredential)
     }
 
     return (
-        <View style={{ marginTop: 200 }}>
+        <View style={styles.root}>
+            <Text style={styles.title}>
+                Win The Day
+            </Text>
             <GoogleSigninButton
                 color={GoogleSigninButton.Color.Light}
                 onPress={handleSignIn}
                 size={GoogleSigninButton.Size.Wide}
-                style={{ height: 48, width: 192 }}
+                style={styles.button}
             />
         </View>
     )
