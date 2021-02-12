2cde31c1182e43f0a9998e8043f501ae114c9bdd
domagoj.vukovic <off.vukovic@gmail.com>
2021-02-12 11:10:39 +0100

    feature: current date UI

--- src/modules/Home/Home.tsx
+++ src/modules/Home/Home.tsx
@@ -1,12 +1,21 @@
+import dayjs from 'dayjs'
 import * as React from 'react'
 import {
     StyleSheet,
+    Text,
     View,
 } from 'react-native'
 
 import { HomeCounters } from './HomeCounters'
 
 const styles = StyleSheet.create({
+    date: {
+        display: 'flex',
+        fontSize: 30,
+        justifyContent: 'flex-start',
+        marginTop: 20,
+        width: '100%',
+    },
     root: {
         alignItems: 'center',
         display: 'flex',
@@ -21,6 +30,9 @@ export const Home: React.FunctionComponent = () => {
     return (
         <View style={styles.root}>
             <HomeCounters />
+            <Text style={styles.date}>
+                {dayjs().format('DD/MM/YYYY')}
+            </Text>
         </View>
     )
 }
