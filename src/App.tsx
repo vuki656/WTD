c5bf41b88f60dc626164c614ef18eff1babcd7f7
domagoj.vukovic <off.vukovic@gmail.com>
2021-02-13 22:14:12 +0100

    feature: sync tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import { GoogleSignin } from '@react-native-community/google-signin'
+import cuid from 'cuid'
 import * as React from 'react'
 
 import { useAuthInit } from './lib/hooks/useAuthInit/useAuthInit'
+import {
+    COLLECTION,
+    connection,
+} from './lib/utils/connection'
+import { getCurrentUser } from './lib/utils/getCurrentUser'
 import { initErrorSuppression } from './lib/utils/initErrorSuppression'
+import { TODAYS_DATE } from './lib/variables/constants'
 import { Home } from './modules/Home'
+import type { TaskType } from './modules/Home/HomeTaskList/HomeTaskList.types'
 import { Login } from './modules/Login'
 
 GoogleSignin.configure({
@@ -12,8 +20,87 @@ GoogleSignin.configure({
 
 initErrorSuppression()
 
+// TODO: TURNING OFF FIREBASE SUBS
 export const App: React.FunctionComponent = () => {
     const { isLoading, isUserAuthenticated } = useAuthInit()
+
+    const user = getCurrentUser()
+
+    /**
+     *  Makes sure that user always has tasks for today from the template list
+     *
+     *  1. Fetch template tasks
+     *  2. Fetch tasks for today
+     *  3. See which template tasks are not present in todays tasks
+     *  4. Create those
+     */
+    const generateTasks = async () => {
+        const tasks: TaskType[] = []
+        const todaysTasks: TaskType[] = []
+
+        await connection(COLLECTION.TASKS)
+            .where('user', '==', user?.uid)
+            .get()
+            .then((results) => {
+                const fetchedTasks: TaskType[] = []
+
+                results.forEach((result) => {
+                    const task = result.data() as TaskType
+
+                    fetchedTasks.push(task)
+                })
+
+                tasks.push(...fetchedTasks)
+            })
+
+        await connection(COLLECTION.TASK_HISTORY)
+            .where('date', '==', TODAYS_DATE)
+            .get()
+            .then((results) => {
+                const fetchedTodaysTasks: TaskType[] = []
+
+                results.forEach((result) => {
+                    const task = result.data() as TaskType
+
+                    fetchedTodaysTasks.push(task)
+                })
+
+                todaysTasks.push(...fetchedTodaysTasks)
+            })
+
+        const tasksToCreate = tasks.reduce((accumulator: TaskType[], task) => {
+            const isCreated = todaysTasks.some((historyTask) => {
+                return historyTask.name === task.name
+            })
+
+            if (isCreated) {
+                return accumulator
+            } else {
+                return [...accumulator, task]
+            }
+        }, [])
+
+        const taskPromises = tasksToCreate.reduce((accumulator: Promise<void>[], task) => {
+            const taskId = cuid()
+
+            const taskJob = connection(COLLECTION.TASK_HISTORY)
+                .doc(taskId)
+                .set({
+                    ...task,
+                    date: TODAYS_DATE,
+                    id: taskId,
+                    isCompleted: false,
+                })
+
+            return [...accumulator, taskJob]
+        }, [])
+
+        void Promise.all(taskPromises)
+    }
+
+    React.useEffect(() => {
+        void generateTasks()
+    }, [])
 
     if (isLoading) {
         return null
--- src/lib/utils/syncStats/syncStats.ts
+++
@@ -1,5 +0,0 @@
-import { getCurrentUser } from '../getCurrentUser/getCurrentUser'
-
-export const syncStats = () => {
-    const user = getCurrentUser()
-}
